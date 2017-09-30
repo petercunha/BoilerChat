cdd3bab96167a3d007be77acd3a44b670ef5cfa0
Peter Cunha <petercunha8@gmail.com>
2017-09-30 13:13:49 -0400

    Make chat more snazzy

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.static(path.join(__dirname, 'frontend')))
 var usernames = {};
 
 // rooms which are currently available in chat
-var rooms = ['room1','room2','room3'];
+var rooms = ['CS 180','CS 240','CS 361'];
 
 io.sockets.on('connection', function (socket) {
 
@@ -25,15 +25,15 @@ io.sockets.on('connection', function (socket) {
 		// store the username in the socket session for this client
 		socket.username = username;
 		// store the room name in the socket session for this client
-		socket.room = 'room1';
+		socket.room = 'CS 180';
 		// add the client's username to the global list
 		usernames[username] = username;
 		// send client to room 1
-		socket.join('room1');
+		socket.join('CS 180');
 		// echo to client they've connected
-		socket.emit('updatechat', 'SERVER', 'you have connected to room1');
+		socket.emit('updatechat', 'SERVER-MSG', 'You have connected to ' + socket.room);
 		// echo to room 1 that a person has connected to their room
-		socket.broadcast.to('room1').emit('updatechat', 'SERVER', username + ' has connected to this room');
+		socket.broadcast.to('room1').emit('updatechat', 'SERVER-MSG', username + ' has connected to this room');
 		socket.emit('updaterooms', rooms, 'room1');
 	});
 
@@ -46,12 +46,12 @@ io.sockets.on('connection', function (socket) {
 	socket.on('switchRoom', function(newroom){
 		socket.leave(socket.room);
 		socket.join(newroom);
-		socket.emit('updatechat', 'SERVER', 'you have connected to '+ newroom);
+		socket.emit('updatechat', 'SERVER-MSG', 'You have connected to ' + newroom);
 		// sent message to OLD room
-		socket.broadcast.to(socket.room).emit('updatechat', 'SERVER', socket.username+' has left this room');
+		socket.broadcast.to(socket.room).emit('updatechat', 'SERVER-MSG', socket.username+' has left this room');
 		// update socket session room title
 		socket.room = newroom;
-		socket.broadcast.to(newroom).emit('updatechat', 'SERVER', socket.username+' has joined this room');
+		socket.broadcast.to(newroom).emit('updatechat', 'SERVER-MSG', socket.username+' has joined this room');
 		socket.emit('updaterooms', rooms, newroom);
 	});
 
@@ -63,7 +63,7 @@ io.sockets.on('connection', function (socket) {
 		// update list of users in chat, client-side
 		io.sockets.emit('updateusers', usernames);
 		// echo globally that this client has left
-		socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
+		socket.broadcast.emit('updatechat', 'SERVER-MSG', socket.username + ' has disconnected');
 		socket.leave(socket.room);
 	});
 });
---
+++ frontend/js/chatroom.js
@@ -0,0 +1,78 @@
+var fullURL = location.protocol + '//' + location.hostname + (location.port ? ':' + location.port : '')
+var socket = io.connect(fullURL)
+var user
+
+// on connection to server, ask for user's name with an anonymous callback
+socket.on('connect', function() {
+	// call the server-side function 'adduser' and send one parameter (value of prompt)
+	// user = prompt("What's your name?")
+	user = nameGenerator()
+	socket.emit('adduser', user)
+	$('#nametag').text(user)
+})
+
+// listener, whenever the server emits 'updatechat', this updates the chat body
+socket.on('updatechat', function(username, data) {
+	// Handle messages from the chatserver
+	if (username == 'SERVER-MSG') {
+		iziToast.show({
+			theme: 'dark',
+			icon: 'icon-person',
+			title: 'Success',
+			message: data,
+			position: 'topCenter', // bottomRight, bottomLeft, topRight, topLeft, topCenter, bottomCenter
+			progressBarColor: 'rgb(0, 255, 184)',
+			timeout: 1900
+		})
+		return
+	}
+
+	// Append message to conversation
+	if (user == username) {
+		$('#conversation').append(`
+        <li class="user">
+            <p>${data}</p>
+        </li>
+        `)
+	} else {
+		$('#conversation').append(`
+        <li>
+            <label>${username}</label>
+            </br>
+            <p>${data}</p>
+        </li>
+        `)
+	}
+})
+
+function switchRoom(room) {
+	socket.emit('switchRoom', room)
+}
+
+// on load of page
+$(function() {
+	// when the client clicks SEND
+	$('#datasend').click(function() {
+		var message = $('#data').val()
+		$('#data').val('')
+		// tell server to execute 'sendchat' and send along one parameter
+		socket.emit('sendchat', message)
+	})
+
+	// when the client hits ENTER on their keyboard
+	$('#data').keypress(function(e) {
+		if (e.which == 13) {
+			$(this).blur()
+			$('#datasend').focus().click()
+			$(this).focus()
+		}
+	})
+})
+
+function nameGenerator() {
+	var animals = ["Alligator", "Anteater", "Armadillo", "Auroch", "Axolotl", "Badger", "Bat", "Beaver", "Buffalo", "Camel", "Chameleon", "Cheetah", "Chipmunk", "Chinchilla", "Chupacabra", "Cormorant", "Coyote", "Crow", "Dingo", "Dinosaur", "Dolphin", "Duck", "Elephant", "Ferret", "Fox", "Frog", "Giraffe", "Gopher", "Grizzly", "Hedgehog", "Hippo", "Hyena", "Jackal", "Ibex", "Ifrit", "Iguana", "Koala", "Kraken", "Lemur", "Leopard", "Liger", "Llama", "Manatee", "Mink", "Monkey", "Narwhal", "Nyan Cat", "Orangutan", "Otter", "Panda", "Penguin", "Platypus", "Python", "Pumpkin", "Quagga", "Rabbit", "Raccoon", "Rhino", "Sheep", "Shrew", "Skunk", "Slow Loris", "Squirrel", "Turtle", "Walrus", "Wolf", "Wolverine", "Wombat"]
+	var adjectives = ["Adorable", "Beautiful", "Clean", "Drab", "Elegant", "Fancy", "Glamorous", "Handsome", "Long", "Magnificent", "Old-fashioned", "Plain", "Quaint", "Sparkling", "Ugliest", "Unsightly", "Wide-eyed", "Alive", "Better", "Careful", "Clever", "Dead", "Easy", "Famous", "Gifted", "Helpful", "Important", "Inexpensive", "Mushy", "Odd", "Powerful", "Rich", "Shy", "Tender", "Uninterested", "Vast", "Wrong", "Agreeable", "Brave", "Calm", "Delightful", "Eager", "Faithful", "Gentle", "Happy", "Jolly", "Kind", "Lively", "Nice", "Obedient", "Proud", "Relieved", "Silly", "Thankful", "Victorious", "Witty", "Zealous", "Angry", "Bewildered", "Clumsy", "Defeated", "Embarrassed", "Fierce", "Grumpy", "Helpless", "Itchy", "Jealous", "Lazy", "Mysterious", "Nervous", "Obnoxious", "Panicky", "Repulsive", "Scary", "Thoughtless", "Uptight", "Worried", "Broad", "Chubby", "Crooked", "Curved", "Deep", "Flat", "High", "Hollow", "Low", "Narrow", "Round", "Shallow", "Skinny", "Square", "Steep", "Straight", "Wide", "Big", "Colossal", "Fat", "Gigantic", "Great", "Huge", "Immense", "Large", "Little", "Mammoth", "Massive", "Miniature", "Petite", "Puny", "Scrawny", "Short", "Small", "Tall", "Teeny", "Teeny-tiny", "Tiny", "Cooing", "Deafening", "Faint", "Hissing", "Loud", "Melodic", "Noisy", "Purring", "Quiet", "Raspy", "Screeching", "Thundering", "Voiceless", "Whispering", "Ancient", "Brief", "Early", "Fast", "Late", "Long", "Modern", "Old", "Old-fashioned", "Quick", "Rapid", "Short", "Slow", "Swift", "Young", "Bitter", "Delicious", "Fresh", "Greasy", "Juicy", "Hot", "Icy", "Loose", "Melted", "Nutritious", "Prickly", "Rainy", "Rotten", "Salty", "Sticky", "Strong", "Sweet", "Tart", "Tasteless", "Uneven", "Weak", "Wet", "Wooden", "Yummy", "Boiling", "Breeze", "Broken", "Bumpy", "Chilly", "Cold", "Cool", "Creepy", "Crooked", "Cuddly", "Curly", "Damaged", "Damp", "Dirty", "Dry", "Dusty", "Filthy", "Flaky", "Fluffy", "Freezing", "Hot", "Warm", "Wet", "Abundant", "Empty", "Few", "Full", "Heavy", "Light", "Many", "Numerous", "Sparse", "Substantial"]
+	var animal = animals[Math.floor(Math.random() * animals.length)]
+	var adjective = adjectives[Math.floor(Math.random() * adjectives.length)]
+	return adjective + " " + animal
+}
