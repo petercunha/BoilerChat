b74fc717fbc67f0e60ddc279460a91dfa2f402f8
Max Drojjin <max.drojjin@gmail.com>
2017-09-30 14:39:37 -0400

    Created log in page and attached typeahead to it

---
+++ frontend/js/typeaheadAutocomplete.js
@@ -0,0 +1,33 @@
+var substringMatcher = function(strs) {
+  return function findMatches(q, cb) {
+    var matches, substringRegex;
+
+    // an array that will be populated with substring matches
+    matches = [];
+
+    // regex used to determine if a string contains the substring `q`
+    substrRegex = new RegExp(q, 'i');
+
+    // iterate through the pool of strings and for any string that
+    // contains the substring `q`, add it to the `matches` array
+    $.each(strs, function(i, str) {
+      if (substrRegex.test(str)) {
+        matches.push(str);
+      }
+    });
+
+    cb(matches);
+  };
+};
+
+var courses = ["CS18000","CS182000"];
+
+$('#Courses .typeahead_Course').typeahead({
+  hint: true,
+  highlight: true,
+  minLength: 1
+},
+{
+  name: 'courses',
+  source: substringMatcher(courses)
+});
