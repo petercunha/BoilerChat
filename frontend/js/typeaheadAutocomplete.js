3fd2b0dc9a97eaaabfff3d78b8613d78b5a76982
Peter Cunha <petercunha8@gmail.com>
2017-09-30 16:14:07 -0400

    AUTOCOMPLETE IS COMPLETE, BABY

--- frontend/js/typeaheadAutocomplete.js
+++ frontend/js/typeaheadAutocomplete.js
@@ -1,27 +1,3 @@
-var substringMatcher = function(strs) {
-  return function findMatches(q, cb) {
-    var matches, substringRegex;
-
-    // an array that will be populated with substring matches
-    matches = [];
-
-    // regex used to determine if a string contains the substring `q`
-    substrRegex = new RegExp(q, 'i');
-
-    // iterate through the pool of strings and for any string that
-    // contains the substring `q`, add it to the `matches` array
-    $.each(strs, function(i, str) {
-      if (substrRegex.test(str)) {
-        matches.push(str);
-      }
-    });
-
-    cb(matches);
-  };
-};
-
-var courses = ["CS18000","CS182000"];
-
 $('#Courses .typeahead_Course').typeahead({
   hint: true,
   highlight: true,
@@ -29,5 +5,11 @@ $('#Courses .typeahead_Course').typeahead({
 },
 {
   name: 'courses',
-  source: substringMatcher(courses)
-});
+  source: function(query, syncResults, asyncResults) {
+      $.get('http://localhost:8080/api/' + query).done(function(data) {
+          for (var i = 0; i < data.length; i++) {
+            asyncResults(data)
+          }
+      })
+  }
+})
