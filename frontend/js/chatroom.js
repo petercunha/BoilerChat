150024d1f6855c341bf6f074b20363920795d384
Peter Cunha <petercunha8@gmail.com>
2017-10-05 18:34:02 -0400

    Fix formatting to conform to https://standardjs.com formatting rules

--- frontend/js/chatroom.js
+++ frontend/js/chatroom.js
@@ -1,124 +1,116 @@
-$(document).ready(function() {
-	start()
+$(document).ready(function () {
+  start()
 })
 
-function start() {
-	var fullURL = location.protocol + '//' + location.hostname + (location.port ? ':' + location.port : '')
-	var socket = io.connect(fullURL)
-	var lastMessageUsername = ''
-	var user
+function start () {
+  var fullURL = location.protocol + '//' + location.hostname + (location.port ? ':' + location.port : '')
+  var socket = io.connect(fullURL)
+  var lastMessageUsername = ''
+  var user
 
-	var uri = document.location.pathname
-	var lastslashindex = uri.lastIndexOf('/')
-	var resultEncoded = uri.substring(lastslashindex + 1)
-	var result = atob(resultEncoded)
+  var uri = document.location.pathname
+  var lastslashindex = uri.lastIndexOf('/')
+  var resultEncoded = uri.substring(lastslashindex + 1)
+  var result = atob(resultEncoded)
 
-	$('#lecturename').text(result.split(':')[0])
-	$('#lectureprofessor').text(result.split(':')[2])
+  $('#lecturename').text(result.split(':')[0])
+  $('#lectureprofessor').text(result.split(':')[2])
 
-	// on connection to server, ask for user's name with an anonymous callback
-	socket.on('connect', function() {
-		// call the server-side function 'adduser' and send one parameter (value of prompt)
-		// user = prompt("What's your name?")
-		user = nameGenerator()
-		socket.emit('adduser', user, resultEncoded)
-		$('#nametag').text(result.split(':')[0] + ' · ' + result.split(':')[2])
-	})
+  // Handle connection to socket server
+  socket.on('connect', function () {
+    user = nameGenerator()
+    socket.emit('adduser', user, resultEncoded)
+    $('#nametag').text(result.split(':')[0] + ' · ' + result.split(':')[2])
+  })
 
-	// listener, whenever the server emits 'updatechat', this updates the chat body
-	socket.on('updatechat', function(username, data) {
+	// Listener, whenever the server emits 'updatechat', this updates the chat body
+  socket.on('updatechat', function (username, data) {
 		// Handle messages from the chatserver
-		if (username == 'SERVER-MSG') {
-			iziToast.show({
-				theme: 'dark',
-				icon: 'icon-person',
-				title: 'Success',
-				message: data,
-				position: 'topCenter', // bottomRight, bottomLeft, topRight, topLeft, topCenter, bottomCenter
-				progressBarColor: 'rgb(0, 255, 184)',
-				timeout: 1900
-			})
-			return
-		}
+    if (username === 'SERVER-MSG') {
+      iziToast.show({
+        theme: 'dark',
+        icon: 'icon-person',
+        title: 'Success',
+        message: data,
+        position: 'topCenter', // bottomRight, bottomLeft, topRight, topLeft, topCenter, bottomCenter
+        progressBarColor: 'rgb(0, 255, 184)',
+        timeout: 1900
+      })
+      return
+    }
 
 		// If the last message was from the same user, don't prepend their username
-		var label = ''
-		if (lastMessageUsername != username) {
-			label = `<label>${username}</label></br>`
-		}
-		lastMessageUsername = username
+    var label = ''
+    if (lastMessageUsername !== username) {
+      label = `<label>${username}</label></br>`
+    }
+    lastMessageUsername = username
 
 		// Append message to conversation
-		if (user == username) {
-			$('#conversation').append(`
+    if (user === username) {
+      $('#conversation').append(`
 	        <li class="user">
 							${label}
 	            <p>${data}</p>
 	        </li>
 	        `)
-		} else {
-			$('#conversation').append(`
+    } else {
+      $('#conversation').append(`
 	        <li>
 	            ${label}
 	            <p>${data}</p>
 	        </li>
 	        `)
-		}
+    }
 
+    var chatbox = document.getElementById('conversation')
+    chatbox.scrollTop = chatbox.scrollHeight
+  })
 
-
-		var chatbox = document.getElementById("conversation")
-		chatbox.scrollTop = chatbox.scrollHeight
-	})
-
-	function switchRoom(room) {
-		socket.emit('switchRoom', room)
-	}
-
-	// on load of page
-	$(function() {
-		// when the client clicks SEND
-		$('#datasend').click(function() {
-			var message = $('#data').val()
+	// On load of page
+  $(function () {
+		// When the client clicks SEND
+    $('#datasend').click(function () {
+      var message = $('#data').val()
 
 			// Prevent bad input
-			if (message.length == 0) return false
-			if (message.length > 500) {
-				iziToast.warning({
-					title: 'Error',
-					message: 'Your message was too long. The maximum length is 500 characters.',
-					position: 'topCenter',
-					timeout: 1900
-				})
-				return false
-			}
+      if (message.length === 0) return false
+      if (message.length > 500) {
+        iziToast.warning({
+          title: 'Error',
+          message: 'Your message was too long. The maximum length is 500 characters.',
+          position: 'topCenter',
+          timeout: 1900
+        })
+        return false
+      }
 
-			$('#data').val('')
-			socket.emit('sendchat', message)
-		})
+      $('#data').val('')
+      socket.emit('sendchat', message)
+    })
 
-		// when the client hits ENTER on their keyboard
-		$('#data').keypress(function(e) {
-			if (e.which == 13) {
-				$(this).blur()
-				$('#datasend').focus().click()
-				$(this).focus()
-			}
-		})
+		// When the client hits ENTER on their keyboard
+    $('#data').keypress(function (e) {
+      if (e.which === 13) {
+        $(this).blur()
+        $('#datasend').focus().click()
+        $(this).focus()
+      }
+    })
 
-		var chatbox = document.getElementById("conversation")
-		chatbox.scrollTop = chatbox.scrollHeight
-	})
+    var chatbox = document.getElementById('conversation')
+    chatbox.scrollTop = chatbox.scrollHeight
+  })
 
-	$('input, select, textarea').on('focus blur', function(event) {
-		$('meta[name=viewport]').attr('content', 'width=device-width,initial-scale=1,maximum-scale=' + (event.type == 'blur' ? 10 : 1));
-	});
+  $('input, select, textarea').on('focus blur', function (event) {
+    $('meta[name=viewport]').attr('content', 'width=device-width,initial-scale=1,maximum-scale=' + (event.type === 'blur' ? 10 : 1))
+  })
 
-	function nameGenerator() {
-		var animals = ["Alligator", "Purdue Pete", "Anteater", "Armadillo", "Badger", "Bat", "Beaver", "Buffalo", "Camel", "Chameleon", "Cheetah", "Chipmunk", "Chinchilla", "Chupacabra", "Dingus", "Coyote", "Crow", "Dingo", "Dinosaur", "Dolphin", "Duck", "Elephant", "Ferret", "Fox", "Frog", "Giraffe", "Gopher", "Grizzly", "Hedgehog", "Hippo", "Hyena", "Jackal", "Ibex", "Iguana", "Koala", "Kraken", "Lemur", "Leopard", "Liger", "Llama", "Manatee", "Mink", "Monkey", "Narwhal", "Orangutan", "Otter", "Panda", "Penguin", "Platypus", "Python", "Pumpkin", "Sea Cucumber", "Rabbit", "Raccoon", "Rhino", "Sheep", "Shrew", "Skunk", "Squirrel", "Turtle", "Walrus", "Wolf", "Wolverine", "Wombat", "Tuna"]
-		var adjectives = ["Adorable", "Beautiful", "Clean", "Drab", "Elegant", "Fancy", "Glamorous", "Handsome", "Long", "Magnificent", "Old-fashioned", "Plain", "Quaint", "Sparkling", "Ugliest", "Unsightly", "Wide-eyed", "Alive", "Better", "Careful", "Clever", "Dead", "Easy", "Famous", "Gifted", "Helpful", "Important", "Inexpensive", "Mushy", "Odd", "Powerful", "Rich", "Shy", "Tender", "Uninterested", "Vast", "Wrong", "Agreeable", "Brave", "Calm", "Delightful", "Eager", "Faithful", "Gentle", "Happy", "Jolly", "Kind", "Lively", "Nice", "Obedient", "Proud", "Relieved", "Silly", "Thankful", "Victorious", "Witty", "Zealous", "Angry", "Wild", "Bewildered", "Clumsy", "Defeated", "Embarrassed", "Fierce", "Grumpy", "Helpless", "Itchy", "Jealous", "Lazy", "Mysterious", "Nervous", "Obnoxious", "Panicky", "Repulsive", "Scary", "Thoughtless", "Uptight", "Worried", "Broad", "Chubby", "Crooked", "Curved", "Deep", "Flat", "High", "Hollow", "Low", "Narrow", "Round", "Shallow", "Skinny", "Square", "Steep", "Dynamic", "Wide", "Big", "Glorious", "Colossal", "Fat", "Gigantic", "Great", "Huge", "Immense", "Large", "Little", "Perturbed", "Mammoth", "Massive", "Miniature", "Petite", "Puny", "Scrawny", "Short", "Small", "Tall", "Teeny", "Teeny-tiny", "Tiny", "Cooing", "Deafening", "Faint", "Hissing", "Loud", "Melodic", "Noisy", "Purring", "Quiet", "Raspy", "Screeching", "Thundering", "Voiceless", "Whispering", "Ancient", "Brief", "Early", "Fast", "Late", "Long", "Modern", "Old", "Old-fashioned", "Quick", "Rapid", "Short", "Slow", "Swift", "Young", "Bitter", "Delicious", "Fresh", "Greasy", "Juicy", "Hot", "Icy", "Loose", "Melted", "Nutritious", "Prickly", "Rainy", "Rotten", "Salty", "Sticky", "Strong", "Sweet", "Tart", "Tasteless", "Uneven", "Weak", "Wet", "Wooden", "Yummy", "Boiling", "Breeze", "Broken", "Bumpy", "Chilly", "Cold", "Cool", "Creepy", "Crooked", "Cuddly", "Curly", "Damaged", "Damp", "Dirty", "Dry", "Dusty", "Filthy", "Flaky", "Fluffy", "Freezing", "Hot", "Warm", "Wet", "Abundant", "Empty", "Few", "Full", "Heavy", "Light", "Many", "Numerous", "Sparse", "Substantial"]
-		var animal = animals[Math.floor(Math.random() * animals.length)]
-		var adjective = adjectives[Math.floor(Math.random() * adjectives.length)]
-		return adjective + " " + animal
-	}
+  function nameGenerator () {
+    var animals = ['Alligator', 'Purdue Pete', 'Anteater', 'Armadillo', 'Badger', 'Bat', 'Beaver', 'Buffalo', 'Camel', 'Chameleon', 'Cheetah', 'Chipmunk', 'Chinchilla', 'Chupacabra', 'Dingus', 'Coyote', 'Crow', 'Dingo', 'Dinosaur', 'Dolphin', 'Duck', 'Elephant', 'Ferret', 'Fox', 'Frog', 'Giraffe', 'Gopher', 'Grizzly', 'Hedgehog', 'Hippo', 'Hyena', 'Jackal', 'Ibex', 'Iguana', 'Koala', 'Kraken', 'Lemur', 'Leopard', 'Liger', 'Llama', 'Manatee', 'Mink', 'Monkey', 'Narwhal', 'Orangutan', 'Otter', 'Panda', 'Penguin', 'Platypus', 'Python', 'Pumpkin', 'Sea Cucumber', 'Rabbit', 'Raccoon', 'Rhino', 'Sheep', 'Shrew', 'Skunk', 'Squirrel', 'Turtle', 'Walrus', 'Wolf', 'Wolverine', 'Wombat', 'Tuna']
+    var adjectives = ['Adorable', 'Beautiful', 'Clean', 'Drab', 'Elegant', 'Fancy', 'Glamorous', 'Handsome', 'Long', 'Magnificent', 'Old-fashioned', 'Plain', 'Quaint', 'Sparkling', 'Ugliest', 'Unsightly', 'Wide-eyed', 'Alive', 'Better', 'Careful', 'Clever', 'Dead', 'Easy', 'Famous', 'Gifted', 'Helpful', 'Important', 'Inexpensive', 'Mushy', 'Odd', 'Powerful', 'Rich', 'Shy', 'Tender', 'Uninterested', 'Vast', 'Wrong', 'Agreeable', 'Brave', 'Calm', 'Delightful', 'Eager', 'Faithful', 'Gentle', 'Happy', 'Jolly', 'Kind', 'Lively', 'Nice', 'Obedient', 'Proud', 'Relieved', 'Silly', 'Thankful', 'Victorious', 'Witty', 'Zealous', 'Angry', 'Wild', 'Bewildered', 'Clumsy', 'Defeated', 'Embarrassed', 'Fierce', 'Grumpy', 'Helpless', 'Itchy', 'Jealous', 'Lazy', 'Mysterious', 'Nervous', 'Obnoxious', 'Panicky', 'Repulsive', 'Scary', 'Thoughtless', 'Uptight', 'Worried', 'Broad', 'Chubby', 'Crooked', 'Curved', 'Deep', 'Flat', 'High', 'Hollow', 'Low', 'Narrow', 'Round', 'Shallow', 'Skinny', 'Square', 'Steep', 'Dynamic', 'Wide', 'Big', 'Glorious', 'Colossal', 'Fat', 'Gigantic', 'Great', 'Huge', 'Immense', 'Large', 'Little', 'Perturbed', 'Mammoth', 'Massive', 'Miniature', 'Petite', 'Puny', 'Scrawny', 'Short', 'Small', 'Tall', 'Teeny', 'Teeny-tiny', 'Tiny', 'Cooing', 'Deafening', 'Faint', 'Hissing', 'Loud', 'Melodic', 'Noisy', 'Purring', 'Quiet', 'Raspy', 'Screeching', 'Thundering', 'Voiceless', 'Whispering', 'Ancient', 'Brief', 'Early', 'Fast', 'Late', 'Long', 'Modern', 'Old', 'Old-fashioned', 'Quick', 'Rapid', 'Short', 'Slow', 'Swift', 'Young', 'Bitter', 'Delicious', 'Fresh', 'Greasy', 'Juicy', 'Hot', 'Icy', 'Loose', 'Melted', 'Nutritious', 'Prickly', 'Rainy', 'Rotten', 'Salty', 'Sticky', 'Strong', 'Sweet', 'Tart', 'Tasteless', 'Uneven', 'Weak', 'Wet', 'Wooden', 'Yummy', 'Boiling', 'Breeze', 'Broken', 'Bumpy', 'Chilly', 'Cold', 'Cool', 'Creepy', 'Crooked', 'Cuddly', 'Curly', 'Damaged', 'Damp', 'Dirty', 'Dry', 'Dusty', 'Filthy', 'Flaky', 'Fluffy', 'Freezing', 'Hot', 'Warm', 'Wet', 'Abundant', 'Empty', 'Few', 'Full', 'Heavy', 'Light', 'Many', 'Numerous', 'Sparse', 'Substantial']
+    var animal = animals[Math.floor(Math.random() * animals.length)]
+    var adjective = adjectives[Math.floor(Math.random() * adjectives.length)]
+    return adjective + ' ' + animal
+  }
 }
--- frontend/js/login.js
+++ frontend/js/login.js
@@ -1,80 +1,77 @@
-$(document).ready(function() {
-
+$(document).ready(function () {
     // Handle enter button click
-    $('#search').keyup(function(e) {
-        var input = $('#search').val()
+  $('#search').keyup(function (e) {
+    var input = $('#search').val()
 
         // Animate when input is first entered
-        if (input == '') {
-            $('#tablediv').hide('slow')
-        } else {
-            $('#tablediv').show('slow')
-        }
+    if (input === '') {
+      $('#tablediv').hide('slow')
+    } else {
+      $('#tablediv').show('slow')
+    }
 
-        if (input.includes(' ') && input.split(' ').length == 2) {
-            input = input.split(' ')
-            if (input[0].length > 0 && input[1].length > 0) {
-                populateTable(input[0], input[1])
-            }
-        } else {
-            var course_data = ""
-            course_data += '<tr>'
-            course_data += '<td>No class with by that ID. Try again with the format "<b>CS 180</b>".</td>'
-            course_data += '</tr>'
-            $('#course_list').empty()
-            $('#course_list').append(course_data)
-        }
-    })
+    if (input.includes(' ') && input.split(' ').length === 2) {
+      input = input.split(' ')
+      if (input[0].length > 0 && input[1].length > 0) {
+        populateTable(input[0], input[1])
+      }
+    } else {
+      var courseData = ''
+      courseData += '<tr>'
+      courseData += '<td>No class with by that ID. Try again with the format "<b>CS 180</b>".</td>'
+      courseData += '</tr>'
+      $('#course_list').empty()
+      $('#course_list').append(courseData)
+    }
+  })
 
     // Populate table with API response
-    function populateTable(course, id) {
+  function populateTable (course, id) {
         // Map to keep track of and prevent adding duplicate objects to the table
-        var map = {}
+    var map = {}
 
-        $.get(`/api/${course}/${id}`, function(data, status) {
-
+    $.get(`/api/${course}/${id}`, function (data, status) {
             // Table buffer
-            var course_data = ""
+      var courseData = ''
 
-            if (data.length == 0) {
-                course_data += '<tr>'
-                course_data += '<td>No class with by that ID. Try again with the format "<b>CS 180</b>".</td>'
-                course_data += '</tr>'
-            }
+      if (data.length === 0) {
+        courseData += '<tr>'
+        courseData += '<td>No class with by that ID. Try again with the format "<b>CS 180</b>".</td>'
+        courseData += '</tr>'
+      }
 
-            for (var i = 0; i < data.length; i++) {
-                var json = data[i]
+      for (var i = 0; i < data.length; i++) {
+        var json = data[i]
 
                 // Validate the JSON object. Sometimes we have issues with corrupted data.
-                if (!json.Subject || !json.Number || !json.Title || !json.Instructor) continue
+        if (!json.Subject || !json.Number || !json.Title || !json.Instructor) continue
 
                 // Grab relevant data from API response
-                let classID = json.Subject + " " + json.Number
-                let className = json.Title
-                let classInstructor = json.Instructor
+        let classID = json.Subject + ' ' + json.Number
+        let className = json.Title
+        let classInstructor = json.Instructor
 
                 // Base64 encode relevant data
-                let url = btoa(classID + ':' + className + ':' + classInstructor)
+        let url = btoa(classID + ':' + className + ':' + classInstructor)
 
                 // Don't add duplicates to the table
-                if (url in map) {
-                    continue
-                }
+        if (url in map) {
+          continue
+        }
                 // Track the object hash to prevent duplicates
-                map[url] = 1;
+        map[url] = 1
 
                 // Add table data to buffer
-                course_data += '<tr>'
-                course_data += '<td>' + classID + '</td>'
-                course_data += '<td>' + className + '</td>'
-                course_data += '<td>' + classInstructor + '</td>'
-                course_data += '<td><a href="/chat/' + url + '" type="button" class="btn btn-primary btn-sm">Join Chat</></td>'
-                course_data += '</tr>'
-            }
+        courseData += '<tr>'
+        courseData += '<td>' + classID + '</td>'
+        courseData += '<td>' + className + '</td>'
+        courseData += '<td>' + classInstructor + '</td>'
+        courseData += '<td><a href="/chat/' + url + '" type="button" class="btn btn-primary btn-sm">Join Chat</></td>'
+        courseData += '</tr>'
+      }
 
-            $('#course_list').empty()
-            $('#course_list').append(course_data)
-
-        })
-    }
+      $('#course_list').empty()
+      $('#course_list').append(courseData)
+    })
+  }
 })
