ae7492d8a8234d1403cb9e0fc699f3866aa943d7
Peter Cunha <petercunha8@gmail.com>
2017-11-06 22:30:28 -0500

    Revert "Add logging to server. Now logs requests and chat messages."
    This reverts commit 6c4d775ccacea9fa12596ab3c48c10407eda082e.

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express')
 var app = express()
-var morgan = require('morgan')
 var http = require('http')
 var server = http.createServer(app)
 var io = require('socket.io').listen(server)
@@ -11,97 +10,95 @@ var url = 'mongodb://localhost:27017/boilerchat'
 const PORT = 80
 
 // Connect to MongoDB
-MongoClient.connect(url, function(err, db) {
-	if (err) throw err
+MongoClient.connect(url, function (err, db) {
+  if (err) throw err
 })
 
 // Launch the server
 server.listen(PORT)
 console.log('Server started on localhost:' + PORT)
 
-// Log all requests
-app.use(morgan('short'))
-
 app.get('/', (req, res, err) => {
-	res.sendFile(path.join(__dirname, 'frontend', 'login.html'))
+  res.sendFile(path.join(__dirname, 'frontend', 'login.html'))
 })
 
 app.get('/chat/:id', (req, res, err) => {
-	res.sendFile(path.join(__dirname, 'frontend', 'index.html'))
+  res.sendFile(path.join(__dirname, 'frontend', 'index.html'))
 })
 
 app.get('/api/:type/:number', (req, res, err) => {
-	MongoClient.connect(url, function(err, db) {
-		if (err) throw err
+  MongoClient.connect(url, function (err, db) {
+    if (err) throw err
 
-		// Ensure that the query is alphanumeric
-		if (!validator.isAlphanumeric(req.params.type) || !validator.isAlphanumeric(req.params.number)) {
-			res.json([])
-			return false
-		}
+    // Ensure that the query is alphanumeric
+    if (!validator.isAlphanumeric(req.params.type) || !validator.isAlphanumeric(req.params.number)) {
+      res.json([])
+      return false
+    }
 
 		// Set max query length
-		if (req.params.type.length > 15 || req.params.number.length > 15) {
-			res.json([])
-			return false
-		}
+    if (req.params.type.length > 15 || req.params.number.length > 15) {
+      res.json([])
+      return false
+    }
 
-		var courseRegex = new RegExp('^' + req.params.type)
-		var numberRegex = new RegExp('^' + req.params.number)
+    var courseRegex = new RegExp('^' + req.params.type)
+    var numberRegex = new RegExp('^' + req.params.number)
 
-		var aggregation = [{
-				$match: {
-					'Abbreviation': {
-						$regex: courseRegex,
-						$options: 'mi'
-					}
-				}
-			},
-			{
-				$unwind: '$Courses'
-			},
-			{
-				$unwind: '$Courses.Classes'
-			},
-			{
-				$match: {
-					'Courses.Number': {
-						$regex: numberRegex,
-						$options: 'mi'
-					}
-				}
-			},
-			{
-				$unwind: '$Courses.Classes.Sections'
-			},
-			{
-				$match: {
-					'Courses.Classes.Sections.Type': 'Lecture'
-				}
-			},
-			{
-				$unwind: '$Courses.Classes.Sections.Meetings'
-			},
-			{
-				$unwind: '$Courses.Classes.Sections.Meetings.Instructors'
-			},
-			{
-				$project: {
-					'_id': 0,
-					'Subject': '$Abbreviation',
-					'Number': '$Courses.Number',
-					'Title': '$Courses.Title',
-					'Instructor': '$Courses.Classes.Sections.Meetings.Instructors.Name'
-				}
-			}
-		]
+    var aggregation = [
+      {
+        $match: {
+          'Abbreviation': {
+            $regex: courseRegex,
+            $options: 'mi'
+          }
+        }
+      },
+      {
+        $unwind: '$Courses'
+      },
+      {
+        $unwind: '$Courses.Classes'
+      },
+      {
+        $match: {
+          'Courses.Number': {
+            $regex: numberRegex,
+            $options: 'mi'
+          }
+        }
+      },
+      {
+        $unwind: '$Courses.Classes.Sections'
+      },
+      {
+        $match: {
+          'Courses.Classes.Sections.Type': 'Lecture'
+        }
+      },
+      {
+        $unwind: '$Courses.Classes.Sections.Meetings'
+      },
+      {
+        $unwind: '$Courses.Classes.Sections.Meetings.Instructors'
+      },
+      {
+        $project: {
+          '_id': 0,
+          'Subject': '$Abbreviation',
+          'Number': '$Courses.Number',
+          'Title': '$Courses.Title',
+          'Instructor': '$Courses.Classes.Sections.Meetings.Instructors.Name'
+        }
+      }
+    ]
 
-		db.collection('classes').aggregate(aggregation).limit(200).toArray(function(err, result) {
-			if (err) throw err
-			res.send(result)
-			db.close()
-		})
-	})
+    db.collection('classes').aggregate(aggregation).limit(200).toArray(function (err, result) {
+      if (err) throw err
+      res.send(result)
+      db.close()
+    })
+  })
 })
 
 // routing
@@ -110,99 +107,89 @@ app.use(express.static(path.join(__dirname, 'frontend')))
 // usernames which are currently connected to the chat
 var usernames = {}
 
-io.sockets.on('connection', function(socket) {
+io.sockets.on('connection', function (socket) {
 	// when the client emits 'adduser', this listens and executes
-	socket.on('adduser', function(username, room) {
+  socket.on('adduser', function (username, room) {
 		// Sanitize input
-		username = validator.escape(username)
-		room = validator.escape(room)
+    username = validator.escape(username)
+    room = validator.escape(room)
 
 		// store the username in the socket session for this client
-		socket.username = username
+    socket.username = username
 		// store the room name in the socket session for this client
-		socket.room = room
+    socket.room = room
 		// add the client's username to the global list
-		usernames[username] = username
+    usernames[username] = username
 		// send client to room 1
-		socket.join(room)
+    socket.join(room)
 
-		// Send the number of online users in a room
-		var onlineUsers = io.sockets.adapter.rooms[room];
-		if (onlineUsers != null && onlineUsers && onlineUsers.length) {
-			// Emit to room
-			socket.broadcast.to(room).emit('updateusers', 'SERVER-MSG', onlineUsers.length)
-			// Emit to user
-			socket.emit('updateusers', 'SERVER-MSG', onlineUsers.length)
-		}
-
-		// echo to client they've connected
-		socket.emit('updatechat', 'SERVER-MSG', 'You have connected to ' + atob(room).split(':')[0])
-		// echo to room 1 that a person has connected to their room
-		socket.broadcast.to(room).emit('updatechat', 'SERVER-MSG', username + ' has connected to this room')
-	})
-
-	// when the client emits 'sendchat', this listens and executes
-	socket.on('sendchat', function(data) {
-		// Sanitize input
-		data = validator.escape(data)
-    room = atob(socket.room).split(':')
-
-    var msg = {
-      message: data,
-      ip: socket.request.connection.remoteAddress,
-      class: room[0],
-      teacher: room[2]
+    // Send the number of online users in a room
+    var onlineUsers = io.sockets.adapter.rooms[room];
+    if (onlineUsers != null && onlineUsers && onlineUsers.length) {
+      // Emit to room
+      socket.broadcast.to(room).emit('updateusers', 'SERVER-MSG', onlineUsers.length)
+      // Emit to user
+      socket.emit('updateusers', 'SERVER-MSG', onlineUsers.length)
     }
 
-    console.log('Message recieved: ' + JSON.stringify(msg));
+		// echo to client they've connected
+    socket.emit('updatechat', 'SERVER-MSG', 'You have connected to ' + atob(room).split(':')[0])
+		// echo to room 1 that a person has connected to their room
+    socket.broadcast.to(room).emit('updatechat', 'SERVER-MSG', username + ' has connected to this room')
+  })
 
-		if (validator.unescape(data).includes('/meme ')) {
-			io.sockets.in(socket.room).emit('updatechat', 'SERVER-MEME', validator.unescape(data).split('/meme ')[1])
-			return
-		}
+	// when the client emits 'sendchat', this listens and executes
+  socket.on('sendchat', function (data) {
+		// Sanitize input
+    data = validator.escape(data)
+
+    if (validator.unescape(data).includes('/meme ')) {
+      io.sockets.in(socket.room).emit('updatechat', 'SERVER-MEME', validator.unescape(data).split('/meme ')[1])
+      return
+    }
 
 		// we tell the client to execute 'updatechat' with 2 parameters
-		io.sockets.in(socket.room).emit('updatechat', socket.username, data)
-	})
+    io.sockets.in(socket.room).emit('updatechat', socket.username, data)
+  })
 
-	socket.on('switchRoom', function(newroom) {
+  socket.on('switchRoom', function (newroom) {
 		// Sanitize input
-		newroom = validator.escape(newroom)
+    newroom = validator.escape(newroom)
 
-		socket.leave(socket.room)
-		socket.join(newroom)
-		socket.emit('updatechat', 'SERVER-MSG', 'You have connected to ' + newroom)
+    socket.leave(socket.room)
+    socket.join(newroom)
+    socket.emit('updatechat', 'SERVER-MSG', 'You have connected to ' + newroom)
 		// sent message to OLD room
-		socket.broadcast.to(socket.room).emit('updatechat', 'SERVER-MSG', socket.username + ' has left this room')
+    socket.broadcast.to(socket.room).emit('updatechat', 'SERVER-MSG', socket.username + ' has left this room')
 		// update socket session room title
-		socket.room = newroom
-		socket.broadcast.to(newroom).emit('updatechat', 'SERVER-MSG', socket.username + ' has joined this room')
-		// socket.emit('updaterooms', rooms, newroom)
-	})
+    socket.room = newroom
+    socket.broadcast.to(newroom).emit('updatechat', 'SERVER-MSG', socket.username + ' has joined this room')
+    // socket.emit('updaterooms', rooms, newroom)
+  })
 
 	// when the user disconnects.. perform this
-	socket.on('disconnect', function() {
+  socket.on('disconnect', function () {
 		// remove the username from global usernames list
-		delete usernames[socket.username]
+    delete usernames[socket.username]
 		// update list of users in chat, client-side
-		io.sockets.emit('updateusers', usernames)
+    io.sockets.emit('updateusers', usernames)
 
-		// Send the number of online users in a room
-		var onlineUsers = io.sockets.adapter.rooms[socket.room];
-		if (onlineUsers != null && onlineUsers && onlineUsers.length) {
-			socket.broadcast.to(socket.room).emit('updateusers', 'SERVER-MSG', onlineUsers.length)
-		}
+    // Send the number of online users in a room
+    var onlineUsers = io.sockets.adapter.rooms[socket.room];
+    if (onlineUsers != null && onlineUsers && onlineUsers.length) {
+      socket.broadcast.to(socket.room).emit('updateusers', 'SERVER-MSG', onlineUsers.length)
+    }
 
 		// echo globally that this client has left
-		socket.broadcast.to(socket.room).emit('updatechat', 'SERVER-MSG', socket.username + ' has disconnected')
-		socket.leave(socket.room)
-	})
+    socket.broadcast.to(socket.room).emit('updatechat', 'SERVER-MSG', socket.username + ' has disconnected')
+    socket.leave(socket.room)
+  })
 })
 
-function atob(str) {
-	return Buffer.from(str, 'base64').toString('ascii')
+function atob (str) {
+  return Buffer.from(str, 'base64').toString('ascii')
 }
 
-function btoa(str) {
-	return Buffer.from(str).toString('base64')
+function btoa (str) {
+  return Buffer.from(str).toString('base64')
 }
